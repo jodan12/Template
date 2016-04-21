fbaf431ccd0e3a28d52a6db641515104baed9c48
JosephDan <josephdanalinsug@hotmail.com>
2016-04-22 00:35:40 +0800

    Initial commit to template repo

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+const sassLoaders = [
+    'css-loader',
+    'postcss-loader',
+    'sass-loader?includePaths[]=' + path.resolve(__dirname, './src/stylesheets')
+];
+
+module.exports = {
+    context: __dirname,
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./src/scripts/entry.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "scripts.min.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: [/\.sass$/, /\.scss$/],
+                loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+            }
+        ]
+    },
+    plugins: debug ? [ new ExtractTextPlugin("[name].css") ] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ],
+};
